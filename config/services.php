6c88fb63260498ddb00b8c6f42847671683a76b0
Fetrus Meilano Ilhamsyah <fetrusmeilano11@.com>
2026-03-14 19:01:29 +0700

    fix: add missing google config and remove token error

--- config/services.php
+++ config/services.php
@@ -22,4 +22,10 @@ return [
         'chat_id'   => env('TELEGRAM_CHAT_ID'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
+
 ];
